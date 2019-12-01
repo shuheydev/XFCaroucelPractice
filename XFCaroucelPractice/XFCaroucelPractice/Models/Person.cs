e2e538b5b167863839ef4e816c0dd5e6061f732a
shuheydev <shuhey.dev@gmail.com>
2019-12-01 22:04:21 +0900

    Add project files.

---
+++ XFCaroucelPractice/XFCaroucelPractice/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XFCaroucelPractice.Models
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public string AvatarUrl => $"https://robohash.org/{FirstName}-{LastName}?set=set4";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ XFCaroucelPractice/XFCaroucelPractice/Views/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XFCaroucelPractice.Models;
+
+namespace XFCaroucelPractice.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Person> People { get; set; } = new ObservableCollection<Person>();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            for(int i=1;i<100;i++)
+            {
+                People.Add(new Person
+                {
+                    FirstName = $"FirstName{i}",
+                    LastName = $"LastName{i}",
+                });
+            }
+        }
+    }
+}
